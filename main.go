6ed0622607b8060434834d0eb2ef342f96407f99
logica0419 <nagami.t.aa@m.titech.ac.jp>
2021-07-07 00:42:34 +0900

    環境変数の読み込みをできるようにした

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print(err)
+	}
+	localPath := os.Getenv("LOCAL_PATH")
+	distPath := os.Getenv("DIST_PATH")
+	fmt.Print(localPath, " ", distPath, "\n")
+}
