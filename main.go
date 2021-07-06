578a6bb418b16c7deb1277ba61e3143c6d581bda
logica0419 <nagami.t.aa@m.titech.ac.jp>
2021-07-07 03:02:54 +0900

    ローカルにおいてファイルを別フォルダに転送(バックアップ)できるようになった

--- main.go
+++ main.go
@@ -2,18 +2,64 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func env_setting() (string, string) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Print(err)
 	}
 	localPath := os.Getenv("LOCAL_PATH")
 	distPath := os.Getenv("DIST_PATH")
-	fmt.Print(localPath, " ", distPath, "\n")
+	return localPath, distPath
+}
+
+func create_bucket(distPath string) string {
+	t := time.Now()
+	bucketName := fmt.Sprintf("s512_local-%d-%d-%d", t.Year(), t.Month(), t.Day())
+	distPath += "/" + bucketName
+	err := os.MkdirAll(distPath, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("Bucket %s successfully created", distPath)
+}
+
+func copy_file(localPath string, distPath string) string {
+	bu_files, err := ioutil.ReadDir(localPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range bu_files {
+		original, err := os.Open(localPath + "/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		copied, err := os.Create(distPath + "/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.Copy(copied, original)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Copied", file.Name())
+	}
+	return fmt.Sprintf("%d files were successfully copied", len(bu_files))
+}
+
+func main() {
+	localPath, distPath := env_setting()
+	log.Println("Backin' up files from", localPath, "to", distPath, "…")
+	mes := create_bucket(distPath)
+	log.Print(mes)
+	mes = copy_file(localPath, distPath)
+	log.Print(mes)
 }
