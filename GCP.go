00d20edc80f3c3c5d0d46985843d1a0bad811b31
logica0419 <nagami.t.aa@m.titech.ac.jp>
2021-07-11 08:39:07 +0900

    ・コードに説明を追加 ・バケットの作成を実装

--- GCP.go
+++ GCP.go
@@ -15,29 +15,56 @@ import (
 
 func create_client(GCPKey string) *storage.Client {
 	ctx := context.Background()
+	// jsonで渡された鍵のサービスアカウントに紐づけられたクライアントを建てる
 	client, err := storage.NewClient(ctx, option.WithCredentialsFile(GCPKey))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return client
 }
 
-func create_bucket(distPath string) string {
+func create_bucket(client storage.Client, projectID string) (*storage.BucketHandle, string) {
+	// "s512_local" + バックアップ日時 をバケット名にする
 	t := time.Now()
 	bucketName := fmt.Sprintf("s512_local-%d-%d-%d", t.Year(), t.Month(), t.Day())
-	distPath += "/" + bucketName
-	err := os.MkdirAll(distPath, 0777)
+
+	// 10秒経ってもバケットが作成されない(タイムアウト)場合自動的にキャンセルするように設定
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	// バケット名とメタデータの設定
+	bucket := client.Bucket(bucketName)
+	bucketAtters := &storage.BucketAttrs{
+		StorageClass: "COLDLINE",
+		Location:     "asia-northeast1",
+		// 生成から90日でバケットを削除
+		Lifecycle: storage.Lifecycle{Rules: []storage.LifecycleRule{
+			{
+				Action:    storage.LifecycleAction{Type: "Delete"},
+				Condition: storage.LifecycleCondition{AgeInDays: 90},
+			},
+		}},
+	}
+
+	// バケットの作成
+	err := bucket.Create(ctx, projectID, bucketAtters)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return fmt.Sprintf("Bucket %s successfully created", bucketName)
+
+	return bucket, fmt.Sprintf("Bucket %s successfully created", bucketName)
 }
 
-func copy_file(localPath string, distPath string) string {
+func copy_file(localPath, distPath string) string {
+	// ローカルのディレクトリ構造を読み込み
 	bu_files, err := ioutil.ReadDir(localPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// ファイルをストレージにコピー
 	for _, file := range bu_files {
 		copy, err := os.Create(distPath + "/" + file.Name())
 		if err != nil {
--- main.go
+++ main.go
@@ -8,23 +8,34 @@ import (
 )
 
 func env_load() (string, string, string) {
+	// .envファイルを読み込み環境変数とする
 	err := godotenv.Load()
 	if err != nil {
 		log.Print(err)
 	}
+
+	// 環境変数を読み込み
 	localPath := os.Getenv("LOCAL_PATH")
 	GCPKey := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	projectID := os.Getenv("PROJECT_ID")
+
 	return localPath, GCPKey, projectID
 }
 
 func main() {
+	// 環境変数の読み込み
 	localPath, GCPKey, projectID := env_load()
 	log.Println("Backin' up files from", localPath, "to", projectID, "on GCP Storage …")
+
+	// クライアントを建てる
 	client := create_client(GCPKey)
 	defer client.Close()
-	mes := create_bucket(distPath)
+
+	// バケットを作成
+	bucket, mes := create_bucket(*client, projectID)
 	log.Print(mes)
+
+	// バケットへファイルをアップロード
 	mes = copy_file(localPath, distPath)
 	log.Print(mes)
 }
